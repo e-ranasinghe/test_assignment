59ec59e8a63968654647f4c64370f3c569538a12
Eric <ericranasinghe@gmail.com>
2020-11-15 21:09:46 +0200

    changed login verification

--- test_assignment/urls.py
+++ test_assignment/urls.py
@@ -20,5 +20,5 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='hello.html'), name='hello'),
+    path('', include('app.urls')),
 ]

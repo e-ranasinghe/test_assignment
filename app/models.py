6c0a18ce17fc6e6e6d61800598539ad92cc4805f
Eric <ericranasinghe@gmail.com>
2020-11-15 19:44:57 +0200

    added post model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.CharField(max_length=30)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    published = models.BooleanField()

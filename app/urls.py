5881771a6c5324eb1f6d971e97e7be4384886baa
Eric <ericranasinghe@gmail.com>
2020-11-15 20:32:06 +0200

    temp commit

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='hello'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='/accounts/login/')
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render(request, 'hello.html')
+
+
+@login_required(login_url='/accounts/login/')
+def post_new(request):
+    form = PostForm()
+    return render(request, 'post.html', {'form': form})

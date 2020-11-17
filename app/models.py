d7c3cf6e80fef995d4689a87758dd33d3d09eb19
Eric <ericranasinghe@gmail.com>
2020-11-18 01:19:47 +0200

    added sidebar bootstrap stuff changed model Post

--- app/models.py
+++ app/models.py
@@ -3,7 +3,11 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    user = models.CharField(max_length=30)
+    user = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    # user = models.CharField(max_length=30)
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)

42c382ebc461cb65be56d942498d8fc5b6c89715
Eric <ericranasinghe@gmail.com>
2020-11-17 16:42:52 +0200

    added page with post detail view

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.hello, name='hello'),
     path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,6 +4,8 @@ from .forms import PostForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 
 @login_required
@@ -21,7 +23,13 @@ def post_new(request):
             post.created_at = timezone.now()
             post.published = True
             post.save()
-            return redirect('hello')
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'app/post_new.html', {'form': form})
+
+
+@login_required
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'app/post_view.html', {'post': post})
